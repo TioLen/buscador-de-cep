4d04ec71b410cb3f4abca842bc5eb4eab4bd3ae5
Gabriel Barros <silvagabr57@gmail.com>
2026-05-04 21:28:57 -0300

    fix: standardize import statements in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { FiSearch } from 'react-icons/fi'
-import './styles.css'
-import api from './services/api'
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+import api from './services/api';
+
 function App() {
 
   // abaixo, é uma chamada ao hook useState do React,
